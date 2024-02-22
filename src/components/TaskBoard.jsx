9c5e4c22f7e4d36bb6a55f125e548e7eecf6929f
nafeezmahdi <nafeezmahdi09@gmail.com>
2024-02-23 01:59:37 +0600

    worked on add and edit modal

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AllTaskList from "./task-board/AllTaskList";
 import SearchTask from "./task-board/SearchTask";
 import TaskActions from "./task-board/TaskActions";
+import AddEditTaskModal from "./task-board/add-edit-task/AddEditTaskModal";
 
 export default function TaskBoard() {
   //creating tasks list data
@@ -16,15 +17,23 @@ export default function TaskBoard() {
   };
   // storing tasks data
   const [tasks, setTasks] = useState([defaultTask]);
+  //creating state for showing addEditTaskModal
+  const [showModal, setShowModal] = useState(false);
+
+  //Add Task Button Function
+  function handleAddTask() {
+    setShowModal(true);
+  }
 
   return (
     <section className="mb-20">
+      {showModal && <AddEditTaskModal />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddTask={handleAddTask} />
           <AllTaskList tasks={tasks} />
         </div>
       </div>
